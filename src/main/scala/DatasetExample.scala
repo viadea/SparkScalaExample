b5afc124ef51e0ace161160dc54d1118d99bd4f8
haozhu <viadeazhu@gmail.com>
2021-01-27 15:28:18 -0800

    add new file

---
+++ src/main/scala/DatasetExample.scala
@@ -0,0 +1,21 @@
+/* Dataset Example  */
+import org.apache.spark.sql.SparkSession
+
+case class Flight(DEST_COUNTRY_NAME: String, ORIGIN_COUNTRY_NAME: String, count: BigInt)
+
+object DatasetExample {
+  def main(args: Array[String]) {
+    
+    val spark = SparkSession.builder.appName("GroupByCSV").getOrCreate()
+    import spark.implicits._
+    
+    val flightsDF = spark.read.parquet("/data/flight-data/parquet/2010-summary.parquet/")
+    val flights = flightsDF.as[Flight]
+
+    flights.filter(flight_row => flight_row.ORIGIN_COUNTRY_NAME != "Canada")
+           .map(flight_row => flight_row)
+           .show(5)
+
+    spark.stop()
+  }
+}
